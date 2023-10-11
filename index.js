b78f6c7be75c1d942c933c522a707b6920a6fac0
swapnil chauhan <selnox93@gmail.com>
2023-10-11 12:47:39 +0530

    CHNAGJ

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 var express = require('express');
+var mysql = require('mysql2');
 var app = express();
+
+var con = mysql.createConnection({
+    host: "localhost",
+    port: '3306',
+    user: "root",
+    password: "",
+    database: 'Register'
+});
+
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected");
+  });
+
 app.get('/', function (req, res) {
-res.send("Welocme to Nodejs!");
-console.log("dddd")
+    res.send("Welocme to Nodejs!");
+    console.log("dddd")
+ 
 });
 app.listen(5000);
